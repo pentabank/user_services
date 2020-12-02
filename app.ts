7fcaab33687d0ba1ac1674f0997511af77aa297b
cheikhgwane <cheikhgwane@gmail.com>
2020-12-02 19:14:18 +0000

    remove addClient from app.ts

--- app.ts
+++ app.ts
@@ -10,14 +10,13 @@ const expressPlayground = require('graphql-playground-middleware-express')
     .default
 
 const PORT = process.env.PORT || 3000
-
 let app = express()
 
 dotenv.config()
 app.use(logger)
 
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+    console.log(`[server]: Server is running at https://localhost:${PORT}`);
 })
 
 app.use('/graphql', graphQLHTTP.graphqlHTTP({ schema: schema }))
