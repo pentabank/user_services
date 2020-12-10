83973f7ca3a64101785affed10c49738f818680e
cheikhgwane <cheikhgwane@gmail.com>
2020-12-10 16:07:57 +0000

    remove merge conflict markers

--- src/controller/Client/Client.test.ts
+++ src/controller/Client/Client.test.ts
@@ -1,9 +1,5 @@
 import { connect, getConnection } from "../../models/db"
-<<<<<<< HEAD
 import { addClient, deleteClientById, findClientByIdOrEmail, updateClient } from "./Client"
-=======
-import { addClient, deleteClientById, findClientById, updateClient } from "./Client"
->>>>>>> feat/user-creation
 
 const good_client = {
     id: "5fca3891090a8f150c56ca7a",
@@ -14,10 +10,6 @@ const good_client = {
     address: "Derkle",
     phoneNumber: "221771974257",
     createdAt: "01/12/2020",
-<<<<<<< HEAD
-=======
-    isActive: false,
->>>>>>> feat/user-creation
     age: 24,
     email: "cheikhgwae@gmail.com",
     password: "test"
@@ -32,10 +24,6 @@ const bad_client = {
     address: "Derkle",
     phoneNumber: "221771974257",
     createdAt: "01/12/2020",
-<<<<<<< HEAD
-=======
-    isActive: false,
->>>>>>> feat/user-creation
     age: 24,
     email: "cheikhgwane@gmail.com",
 }
@@ -48,48 +36,12 @@ const bad_client_2 = {
     address: "Derkle",
     phoneNumber: "221771974257",
     createdAt: "01/12/2020",
-<<<<<<< HEAD
-=======
-    isActive: false,
->>>>>>> feat/user-creation
     age: 24,
     password: "",
     email: "cheikhgwe@gmail.com",
     dummy: "dummy"
 }
 
-<<<<<<< HEAD
-=======
-describe("Client insertion tests", () => {
-
-    beforeAll(async () => connect())
-    afterAll(async () => {
-        let db = getConnection()
-        db.close()
-    })
-
-    it('Should insert a new client', async () => {
-        let res = await addClient(null, { good_client })
-        expect(res.code).toBe(200)
-    })
-
-    it('Should fail when inserting a new client', async () => {
-        let res = await addClient(null, { bad_client })
-        expect(res.code).toBe(400)
-    })
-
-    it("Passing an empty object", async () => {
-        let res = await addClient(null, { client: {} })
-        expect(res.code).toBe(400)
-    })
-
-    it("Passing an object with more attributes", async () => {
-        let res = await addClient(null, { bad_client_2 })
-        expect(res.code).toBe(400)
-    })
-})
-
->>>>>>> feat/user-creation
 describe("Find client by Id test", () => {
 
     beforeAll(async () => {
@@ -103,19 +55,11 @@ describe("Find client by Id test", () => {
     })
 
     it("Should retrieve a user given his id", async () => {
-<<<<<<< HEAD
         await expect(findClientByIdOrEmail(null, { id: "5fca2c4b6b952f85fe45a" })).rejects.toThrow()
     })
 
     it("Should retrieve a user given his id", async () => {
         await expect(findClientByIdOrEmail(null, { id: "5fca3891090a8f150c56ca7a" })).resolves.toBeDefined()
-=======
-        await expect(findClientById(null, { id: "5fca2c4b6b952f85fe45a" })).rejects.toThrow()
-    })
-
-    it("Should retrieve a user given his id", async () => {
-        await expect(findClientById(null, { id: "5fca3891090a8f150c56ca7a" })).resolves.toBeDefined()
->>>>>>> feat/user-creation
     })
 
 })
@@ -137,11 +81,7 @@ describe("Update client", () => {
     })
 
     it("Update should pass", async () => {
-<<<<<<< HEAD
         await expect(findClientByIdOrEmail(null, { client: good_client })).resolves.toBeDefined()
-=======
-        await expect(findClientById(null, { client: good_client })).resolves.toBeDefined()
->>>>>>> feat/user-creation
     })
 
 })
@@ -164,11 +104,7 @@ describe("Delete client", () => {
     })
 
     it("Deletion should pass", async () => {
-<<<<<<< HEAD
         await expect(findClientByIdOrEmail(null, { id: "5fca3891090a8f150c56ca7z" })).rejects.toThrow()
-=======
-        await expect(findClientById(null, { id: "5fca3891090a8f150c56ca7z" })).rejects.toThrow()
->>>>>>> feat/user-creation
     })
 
 })
